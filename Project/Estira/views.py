7fb9d0643c5b7b2799f0e2b2f38ecd0d661fd677
dennielestira <152932072+dennielestira@users.noreply.github.com>
2024-10-10 13:16:24 +0800

    Delete Project directory

--- Project/Estira/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class HomePageView(TemplateView):
-    template_name = 'Estira/home.html'
-class AboutPageView(TemplateView):
-    template_name = 'Estira/about.html'
-class SkillsPageView(TemplateView):
-    template_name = 'Estira/skills.html'
